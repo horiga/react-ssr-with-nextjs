27b969a23774f5b4e4210c7c509157e4f19514a5
horiga <h.horiga@gmail.com>
2019-06-09 19:13:28 +0900

    d2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 function Home() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      Welcome to Next.js!<br/>
+      Click{' '}
+      <Link href="/about"><a>here</a></Link>{' '}
+      to read more
+    </div>);
 }
 
 export default Home;
